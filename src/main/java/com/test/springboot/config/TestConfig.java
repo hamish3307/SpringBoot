3741fc75cf0c6442963fa9c827567e17588bf0e8
hamish3307 <345943314@qq.com>
2020-12-03 16:56:37 +0800

    增加面试测试

---
+++ src/main/java/com/test/springboot/config/TestConfig.java
@@ -0,0 +1,12 @@
+package com.test.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @描述:
+ * @Date: 2020/11/15 3:06 下午
+ * @Author: hha
+ */
+@Configuration
+public class TestConfig {
+}
